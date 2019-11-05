9f47e59c78acd1e23e4b55ae504b2be07163dbe6
Abhijeet Krishnan <abhijeet.krishnan@gmail.com>
2019-11-04 23:08:33 -0500

    Change algorithm

--- Codechef/NOV19B/PHCUL.py
+++ Codechef/NOV19B/PHCUL.py
@@ -1,4 +1,4 @@
-from itertools import zip_longest
+import itertools
 import math
 
 def dist(u, v):
@@ -8,7 +8,7 @@ def dist(u, v):
 
 def grouper(iterable, n=2, fillvalue=None):
     args = [iter(iterable)] * n
-    return zip_longest(*args, fillvalue=fillvalue)
+    return itertools.zip_longest(*args, fillvalue=fillvalue)
 
 def min_distance(x, A, B, C):
     min_c = min(grouper(C), key=lambda c: dist(c, x))
@@ -22,8 +22,21 @@ if __name__ == '__main__':
     for test in range(t):
         x = tuple(map(int, input().split()))
         n, m, k = map(int, input().split())
-        N = list(map(int, input().split()))
-        M = list(map(int, input().split()))
-        K = list(map(int, input().split()))
+        N = list(grouper(list(map(int, input().split()))))
+        M = list(grouper(list(map(int, input().split()))))
+        K = list(grouper(list(map(int, input().split()))))
 
-        print(min(min_distance(x, N, M, K), min_distance(x, M, N, K)))
+        min_n2k = {}
+        for i in range(n):
+            min_n2k[i] = K.index(min(K, key=lambda k: dist(k, N[i])))
+        min_m2k = {}
+        for j in range(m):
+            min_m2k[j] = K.index(min(K, key=lambda k: dist(k, M[j])))
+        
+        min_distance = dist(N[0], x) + dist(M[0], N[0]) + dist(K[0], M[0])
+        for i in range(n):
+            for j in range(m):
+                min_distance = min(min_distance, dist(N[i], x) + dist(M[j], N[i]) + dist(K[min_m2k[j]], M[j]))
+                min_distance = min(min_distance, dist(M[j], x) + dist(N[i], M[j]) + dist(K[min_n2k[i]], N[i]))
+        print(min_distance)
+            
