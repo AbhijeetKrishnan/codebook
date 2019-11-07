e9983ed491a11705a0a764a90790e9df9949a963
Abhijeet Krishnan <abhijeet.krishnan@gmail.com>
2019-11-07 02:16:51 -0500

    Add submission #4

--- Codechef/NOV19B/LSTBTF.py
+++ Codechef/NOV19B/LSTBTF.py
@@ -12,6 +12,22 @@ poss.append({
     80: '9',
 })
 
+def digitSum(n):
+    r = 0
+    while n:
+        r, n = r + ((n % 10) ** 2), n // 10
+    return r
+
+def isPerfectSquare(n):
+    return math.sqrt(n) - math.floor(math.sqrt(n)) == 0
+
+def isBeautifulNumber(n):
+    digit_sum = digitSum(n)
+    # print(f'digitSum of {n} is {digit_sum}')
+    if isPerfectSquare(digit_sum):
+        return True
+    return False
+
 def squares(a, b):
     start = math.ceil(math.sqrt(a))
     end = math.floor(math.sqrt(b))
@@ -29,6 +45,8 @@ for i in range(2, 40):
             elif new_value < poss[i][new_key]:
                 poss[i][new_key] = new_value
 
+# print(poss)
+
 if __name__ == '__main__':
     t = int(input())
     for test in range(t):
@@ -37,15 +55,29 @@ if __name__ == '__main__':
         n = digs
         max_change = 81 * n
         for i in range(1, digs + 1):
+            min_ans = '9' * digs
             for square in squares(n, max_change):
                 diff = square - n
+                # print(f'Square: {square}, diff: {diff}')
+                if diff > i * 80: # change to all 9s
+                    break
                 if diff == 0:
-                    print('1' * digs)
                     found = True
-                    break
+                    ans = '1' * digs
+                    min_ans = min(min_ans, ans)
+                # print(f'diff = {diff} in poss[{i}] = {diff in poss[i]}')
                 if diff in poss[i]:
-                    print(('1' * (digs - len(poss[i][diff])) + poss[i][diff]))
+                    # print(f'While updating ans: diff = {diff} in poss[{i}] = {diff in poss[i]}')
                     found = True
-                    break
+                    ans = ('1' * (digs - len(poss[i][diff])) + poss[i][diff])
+                    
+                    min_ans = min(min_ans, ans)
+                    # print(ans, min_ans)
             if found:
+                print(min_ans)
+                # if not isBeautifulNumber(int(min_ans)):
+                #     print(digs, min_ans)
+                # for poss_ans in range(int(min_ans) - 1, int('1' * digs), -1):
+                #     if '0' not in str(poss_ans) and isBeautifulNumber(poss_ans):
+                #         print(digs, min_ans, poss_ans, digitSum(poss_ans))
                 break
