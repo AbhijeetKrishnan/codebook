7edac96c947899e7cad98cb96c88b3856fe87950
Abhijeet Krishnan <abhijeet.krishnan@gmail.com>
2019-11-09 21:38:13 -0500

    Add submission #5

--- Codechef/NOV19B/FAILURE.cpp
+++ Codechef/NOV19B/FAILURE.cpp
@@ -1,58 +1,39 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
+#include <deque>
+#include <set>
 
 using namespace std;
 
-enum Colour { WHITE, GREY, BLACK };
-
-template<typename T>
-void debug(vector<T>& v, string s) {
-    cout << s << "\n";
-    for (int i = 0; i < v.size(); i++) {
-        cout << i << " ";
-    }
-    cout << "\n";
-    for (auto ele: v) {
-        cout << ele << " ";
-    }
-    cout << "\n";
-}
-
-void dfs(vector<vector<int>>& g, int root, int par, vector<int>& parent, vector<Colour>& colour, int skip, bool& contains_cycle) {
-    if (colour[root] == Colour::BLACK) {
-        return;
-    }
-    else if (colour[root] == Colour::GREY) {
-        // cout << "root: " << root << " parent: " << par << "\n";
-        contains_cycle = true;
-        return;
-    }
-    parent[root] = par;
-    colour[root] = Colour::GREY;
-    for (int v: g[root]) {
-        if (v == parent[root] or v == skip) {
-            continue;
-        }
-        dfs(g, v, root, parent, colour, skip, contains_cycle);
-    }
-    colour[root] = Colour::BLACK;
-}
-
-bool contains_cycle(vector<vector<int>>& g, int n, int skip) {
+bool contains_cycle(vector<set<int>>& g, int n, int skip) {
     vector<int> parent(n);
-    vector<Colour> colour(n, Colour::WHITE);
-    vector<pair<int, int>> stack;
-    bool _contains_cycle = false;
+    vector<bool> seen(n, false);
+    deque<int> q;
     for (int root = 0; root < n; root++) {
-        if (colour[root] == Colour::BLACK or root == skip) {
+        if (seen[root] or root == skip) {
             continue;
         }
-        dfs(g, root, root, parent, colour, skip, _contains_cycle);
+        parent[root] = root;
+        seen[root] = true;
+        q.push_back(root);
+        while (not q.empty()) {
+            int u = q.front(); q.pop_front();
+            for (int v: g[u]) {
+                if (v == skip) {
+                    continue;
+                }
+                if (not seen[v]) {
+                    seen[v] = true;
+                    parent[v] = u;
+                    q.push_back(v);
+                }
+                else if (seen[v] and parent[u] != v) {
+                    return true;
+                }
+            }
+        }
     }
-    // debug(parent, "Parent");
-    // debug(colour, "Colour");
-    return _contains_cycle;
+    return false;
 }
 
 int main() {
@@ -61,23 +42,21 @@ int main() {
     for (int test = 0; test < t; test++) {
         int n, m;
         cin >> n >> m;
-        vector<vector<int>> g(n);
+        vector<set<int>> g(n);
         for (int i = 0; i < m; i++) {
             int u, v;
             cin >> u >> v;
             u--, v--;
-            g[u].push_back(v);
-            g[v].push_back(u);
+            g[u].insert(v);
+            g[v].insert(u);
         }
         
         if (not contains_cycle(g, n, -1)) {
             cout << "-1\n";
             continue;
         }
-
         int failure = -1;
         for (int i = 0; i < n; i++) {
-            // cout << "i: " << i << "\n";
             if (not contains_cycle(g, n, i)) {
                 failure = i + 1;
                 break;
